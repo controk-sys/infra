363dad1f1d6b1a2eb98ca97760049cb7e1305d8c
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-03 09:50:33 -0300

    Update pull_projects.sh

--- pull_projects.sh
+++ pull_projects.sh
@@ -2,6 +2,13 @@ function clone () {
     git clone https://github.com/jourdanrodrigues/controk-${1}.git ${1}
 }
 
-clone webservice
+webservice_folder=webservice
+
+clone ${webservice_folder}
+virtualenv -p /usr/bin/python3.5 ${webservice_folder}/venv/
+source ${webservice_folder}/venv/bin/activate
+python ${webservice_folder}/manage.py collectstatic --no-input
+deactivate
+
 clone frontend-web
 clone socket
