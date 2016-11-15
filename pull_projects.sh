974fd437edf5bc2d29a790483c6785c70f6dce9c
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-15 18:52:07 -0300

    Took out NGINX service due to Django update; updated containers names and postgres db version; took out local configuration for "webservice".

--- pull_projects.sh
+++ pull_projects.sh
@@ -2,13 +2,6 @@ function clone () {
     git clone https://github.com/jourdanrodrigues/controk-${1}.git ${1}
 }
 
-webservice_folder=webservice
-
-clone ${webservice_folder}
-virtualenv -p /usr/bin/python3.5 ${webservice_folder}/venv/
-source ${webservice_folder}/venv/bin/activate
-python ${webservice_folder}/manage.py collectstatic --no-input
-deactivate
-
+clone webservice
 clone frontend-web
 clone socket
